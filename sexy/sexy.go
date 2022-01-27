ad2b52acbd287d6602b2044934dd904583d9ae48
eslerkang <eslerkang@gmail.com>
2022-01-27 07:48:22 +0000

    Example of Go routine

---
+++ sexy/sexy.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c := make(chan string)
+	people:=[]string{"kang", "jun", "haha", "dada"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+
+	for i:=0; i<len(people); i++ {
+		fmt.Println(<-c)
+	}
+}
+
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second*5)
+	c <- person + " is Sexy"
+}
